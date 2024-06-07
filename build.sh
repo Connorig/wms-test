2b5b5d6006881bbaec687998df2c49da09e64baf
connor-g <2417039231@qq.com>
2024-06-07 16:45:25 +0800

    update 6

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ MainPath="cmd/main/main.go"
 Org="thingple"
 
 echo "================================git"
+git branch
 git rev-list --tags --max-count=1
 git describe --tags $(git rev-list --tags --max-count=1)
 echo "================================git"
